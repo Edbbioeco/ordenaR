ff215d8c40ac735bb6852da43fefa37a03eefa36
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-15 17:08:28 -0300

    Script to git Commands

--- comandos_git.R
+++ comandos_git.R
@@ -2,13 +2,19 @@
 
 library(gert)
 
-# Selecionando o arquivo ----
+# Selecionando arquivos aptos ----
 
-gert::git_add(list.files(pattern = "comandos")) |> as.data.frame()
+gert::git_status() |>
+  as.data.frame()
+
+# Adicionando arquivo ----
+
+gert::git_add(list.files(pattern = "comandos_git.R")) |>
+  as.data.frame()
 
 # Commitando ----
 
-gert::git_commit("Script to git commands")
+gert::git_commit("Script to git Commands")
 
 # Pushando ----
 
@@ -16,4 +22,10 @@ gert::git_push(remote = "origin", force = TRUE)
 
 # Pullando ----
 
-gert::git_pull()
+gert::git_pull(remote = "origin")
+
+# resetando ----
+
+gert::git_reset_mixed()
+
+gert::git_reset_soft("HEAD~1")
